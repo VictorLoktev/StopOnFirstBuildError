2709e6e192383d52458493349de84eede5d2c5d4
Виктор Локтев <loktev@mineral.ru>
2022-05-25 08:53:26 +0300

    Some cosmetic fixes: README.md, sign key, version, dist...

--- StopOnFirstBuildError2019/Properties/AssemblyInfo.cs
+++ StopOnFirstBuildError2019/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.3.1.0")]
+[assembly: AssemblyFileVersion("2.3.1.0")]
 
